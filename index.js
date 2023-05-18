ea401c15ef631191f3a224f3a80c02e541118f42
Edgard3033 <ed233094@outlook.com>
2023-05-18 10:58:49 -0400

    installed morgan

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const PORT = 3000;
 const express = require("express");
 const server = express();
+const morgan = require("morgan");
+server.use(morgan("dev"));
+
+server.use(express.json());
 
 const apiRouter = require("./api");
 server.use("/api", apiRouter);
