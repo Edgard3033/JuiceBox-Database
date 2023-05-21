f853e171a0cf8d1a44f288bf93e3a9377421b992
Edgard3033 <ed233094@outlook.com>
2023-05-21 12:49:34 -0400

    Completed GET/api/tags/:tagName/posts

--- api/tags.js
+++ api/tags.js
@@ -7,7 +7,7 @@ tagsRouter.use((req, res, next) => {
   next();
 });
 
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.get("/", async (req, res) => {
   const tags = await getAllTags();
@@ -15,6 +15,18 @@ tagsRouter.get("/", async (req, res) => {
   res.send({
     tags: [],
   });
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  const { tagName } = req.params;
+
+  try {
+    const posts = await getPostsByTagName(tagName);
+    //send an object to the client { post: the post }
+    res.send({ posts });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
